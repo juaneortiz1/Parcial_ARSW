6421aac9eaf86597b2b0621df3b53240e596db0b
Juanes <juaneortiz1@gmail.com>
2024-07-02 08:57:02 -0500

    First commit

---
+++ src/test/java/edu/escuelaing/arsw/ASE/app/WebServer/WebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.ASE.app.WebServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
